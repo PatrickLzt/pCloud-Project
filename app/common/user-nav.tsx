7e37348834a8f88e11311888d20d03c7e2c3fdfb
Patrick Lorenzeti <patrick.lorenzeti@outlook.com>
2023-09-24 18:41:54 -0300

    feat: User menu dropdown

---
+++ app/common/user-nav.tsx
@@ -0,0 +1,96 @@
+"use client"
+import { User, createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation"
+
+import { UserIcon } from "lucide-react"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function UserNav() {
+
+    const [user, setUser] = useState<User | null>()
+    const router = useRouter()
+    const supabase = createClientComponentClient()
+
+    const getUser = async () => {
+
+        const { data: { user }, error } = await supabase.auth.getUser()
+
+        if (error) {
+            console.error("Profile", error)
+        }
+        else {
+            setUser(user)
+        }
+    }
+
+    useEffect(() => {
+        getUser()
+    })
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut()
+        router.refresh()
+    }
+
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                        <Avatar className="h-8 w-8">
+                            <AvatarImage src="/avatars/avatar_2.png" alt="pCloud" />
+                            <AvatarFallback>RS</AvatarFallback>
+                        </Avatar>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                    <DropdownMenuLabel className="font-normal">
+                        <div className="flex flex-col space-y-1">
+                            <p className="text-sm font-medium leading-none">shadcn</p>
+                            <p className="text-xs leading-none text-muted-foreground">
+                                m@example.com
+                            </p>
+                        </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                            Profile
+                            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            Billing
+                            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            Settings
+                            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>New Team</DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        Log out
+                        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -1,6 +1,31 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { RedirectType, redirect } from "next/navigation"
+import { cookies } from "next/headers"
+import UserNav from "../common/user-nav"
 
 export default async function UserApp() {
+
+    let loggedIn = false
+
+    try {
+        const supabase = createServerComponentClient({ cookies })
+        const { data: { session }, error } = await supabase.auth.getSession()
+
+        if (session) {
+            loggedIn = true
+        }
+    } catch (error) {
+        console.log("User_App", error)
+    }
+    finally {
+        if (!loggedIn) {
+            redirect("/", RedirectType.replace)
+        }
+    }
+
     return (
-        <div>Logged In</div>
+        <div>
+            <UserNav />
+        </div>
     )
 }
